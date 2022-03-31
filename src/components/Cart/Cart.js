98498df3bde3e7e7e4221550a58cf0305552709a
Mazdul1000 <mazdul1000@gmail.com>
2022-04-01 01:23:39 +0600

    modify

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,37 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = (props) => {
-    const cart = props.cart;
-   let totalPrice = 0;
-
-   cart.map(product=>totalPrice += parseInt(product.price))
-      
     
+ const cart = props.cart;
+ 
+
+   let totalPrice = 0;
+   let shipping = 0;
+   let grandTotal = 0;
+   let quantity = 0;
+
+   for(const product of cart){    
+   quantity = quantity+ product.quantity;
+       totalPrice = totalPrice+ (product.price*product.quantity);
+
+       shipping += parseInt(product.shipping);
+    }
+      
+    let tax = parseFloat((totalPrice*0.1).toFixed(2));
+    grandTotal = totalPrice+shipping+tax;
+
    
 
-    console.log(props)
+   
     return (
-        <div>
-             <h3> Order summary</h3>
-             <h2>Selected items: {cart.length}</h2>
+        <div className='cart'>
+             <h2> Order summary</h2>
+             <h3>Selected items: {quantity}</h3>
              <h3>Total: ${totalPrice}</h3>
+             <p>Shipping: ${shipping}</p>
+             <p>Tax: ${tax}</p>
+             <h2>Grand Total: ${grandTotal} </h2>
         </div>
     );
 };
